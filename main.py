051bf406d905b165b8502871f2ee68555f7762fd
Panthut Ketphan <panthat.k@gmail.com>
2024-04-11 14:11:16 +0700

    add status with notion url and add empty readme.md

---
+++ main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+
+class App:
+    def __init__(self) -> None:
+        self.root = Tk()
+        self.root.title("Depth Gpu Comparison")
+        self.root.geometry("800x600")
+
+    def run(self):
+        self.root.mainloop()
