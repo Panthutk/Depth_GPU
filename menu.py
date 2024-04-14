08fb29435e56a2a9ec602c373cf73df79027c40c
Panthut Ketphan <panthat.k@gmail.com>
2024-04-14 15:16:32 +0700

    done basic button wait for done histrogram

--- menu.py
+++ menu.py
@@ -12,12 +12,33 @@ class Menu:
     def __init__(self) -> None:
         self.root = Tk()
         self.root.title("Depth Gpu Comparison")
-        self.root.geometry("800x600")
+        self.root.geometry("1000x600")
+        self.read_csv()
+        self.menu_page()
 
         self.run()
 
     def read_csv(self):
-        self.df = pd.read_csv('Clean_gpu_spec_v6.csv')
+        self.df = pd.read_csv('Clean_gpu_specs_v6.csv')
+
+    def menu_page(self):
+        self.label = Label(
+            self.root, text="DGC", font=("Arial", 50)).grid(row=0, column=0, sticky="nsew")
+        self.last_update = Label(
+            self.root, text=f'lasted update on {int(max(self.df["releaseYear"]))}', font=("Arial", 10)).grid(row=1, column=0, sticky="nsew")
+        self.compare_button = Button(
+            self.root, text="Compare Mode").grid(row=2, column=0, sticky="nsew")
+        self.detail_button = Button(
+            self.root, text="Detail Mode").grid(row=3, column=0, sticky="nsew")
+
+        # configure column
+        self.root.columnconfigure(0, weight=1)
+
+        # configure row
+        self.root.rowconfigure(0, weight=2)
+        self.root.rowconfigure(1, weight=1)
+        self.root.rowconfigure(2, weight=3)
+        self.root.rowconfigure(3, weight=3)
 
     def run(self):
         self.root.mainloop()
